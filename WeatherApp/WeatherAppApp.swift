5f04544554bd2f7474d145fcf0e4a3ea9b25124b
Nikolai Khudiakov <nikolaikhud@gmail.com>
2024-09-18 22:29:58 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Nikolai Khudiakov on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
